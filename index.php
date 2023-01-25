67437b34f0eb1ec0d2bda74c06c87c9dfaaf1dc1
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-01-25 15:42:38 +0100

    Print datas from array in to table

--- index.php
+++ index.php
@@ -6,54 +6,89 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
-    <?php
-    $hotels = [
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-    ];
+    <section class="container">
+        <?php
+        $hotels = [
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+        ];
 
-    echo "<pre>";
-    foreach ($hotels as $hotel) {
-        echo "\n", $hotel['name'], "\t\t", $hotel['description'], "\t\t", $hotel['parking'], "\t\t", $hotel['vote'], "\t\t", $hotel['distance_to_center'];
-        echo "\n";
-    };
-    ?>
+        /*
+        echo "<div>";
+        foreach ($hotels as $hotel) {
+            echo "\n", $hotel['name'], "\t\t", $hotel['description'], "\t\t", $hotel['parking'], "\t\t", $hotel['vote'], "\t\t", $hotel['distance_to_center'];
+            echo "\n";
+        };
+        echo "</div>";
+*/
+
+        echo '<table class="table">';
+        echo '<tbody>';
+        foreach ($hotels as $hot) {
+            echo '<tr>';
+            echo '<th scope="row">';
+            echo '.';
+            echo '</th>';
+            echo '<td>';
+            echo $hot['name'];
+            echo '</td>';
+            echo '<td>';
+            echo $hot['description'];
+            echo '</td>';
+            echo '<td>';
+            echo $hot['parking'];
+            echo '</td>';
+            echo '<td>';
+            echo $hot['vote'];
+            echo '<td>';
+            echo $hot['distance_to_center'];
+            echo '</td>';
+            echo '</td>';
+            echo '</tr>';
+        };
+        echo '</tbody>';
+        echo '</table>';
+
+        ?>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
 </html>
